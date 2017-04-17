7412c2346031c7af8fa47e6ca4284be859024c30
허현 <kkiruru@gmail.com>
2017-04-17 20:05:24 +0900

    [허현/PF개발2팀]
    stdin을 통한 값입력 받기 구현

--- Oversized Pancake Flipper/OversizedPancakeFlipper.go
+++ Oversized Pancake Flipper/OversizedPancakeFlipper.go
@@ -1,12 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
 
-	for i := 0; i < 100; i++ {
-		fmt.Print("Case #", i, " : "+flipping(""), "\n")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	scanner.Scan()
+	line := scanner.Text()
+	fmt.Print("line ", line, "\n")
+
+	i := 1
+	for scanner.Scan() {
+		line = scanner.Text()
+		fmt.Print("Case #", i, " : "+flipping(""), " line ", line, "\n")
+		i++
 	}
+
 }
 
 func flipping(pancake string) string {
