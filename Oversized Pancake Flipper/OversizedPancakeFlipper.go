1c1d6b177928afc297c5fe0c5f928522b0a0b9ee
허현 <kkiruru@gmail.com>
2017-04-30 15:45:26 +0900

    [허현/PF개발3팀] 풀이 및 예제 파일 추가.

--- Oversized Pancake Flipper/OversizedPancakeFlipper.go
+++ Oversized Pancake Flipper/OversizedPancakeFlipper.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -11,18 +13,50 @@ func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 
 	scanner.Scan()
-	line := scanner.Text()
-	fmt.Print("line ", line, "\n")
 
 	i := 1
 	for scanner.Scan() {
-		line = scanner.Text()
-		fmt.Print("Case #", i, " : "+flipping(""), " line ", line, "\n")
+		line := scanner.Text()
+		parts := strings.Split(line, " ")
+
+		pancake := parts[0]
+		sizeOfFlipper, _ := strconv.Atoi(parts[1])
+
+		fmt.Print("Case #", i, ": "+flipping([]rune(pancake), sizeOfFlipper), "\n")
 		i++
 	}
-
 }
 
-func flipping(pancake string) string {
-	return "IMPOSIBBLE"
+func flipping(pancakes []rune, sizeOfFlipper int) string {
+
+	var limit int = len(pancakes) - sizeOfFlipper
+	var flippingCount = 0
+
+	for i := 0; i <= limit; i++ {
+		if pancakes[i] == '-' {
+			flippingCount++
+			for j := 0; j < sizeOfFlipper; j++ {
+				if pancakes[i+j] == '-' {
+					pancakes[i+j] = '+'
+				} else {
+					pancakes[i+j] = '-'
+				}
+			}
+		}
+	}
+
+	if isSunnySideUp(pancakes) {
+		return strconv.FormatInt(int64(flippingCount), 10)
+	} else {
+		return "IMPOSSIBLE"
+	}
+}
+
+func isSunnySideUp(pancakes []rune) bool {
+	for _, r := range pancakes {
+		if r == '-' {
+			return false
+		}
+	}
+	return true
 }
