dfc353e703f98a0a52a82408c83988a4787570b6
허현 <kkiruru@gmail.com>
2019-04-13 13:29:08 +0900

    - 1. 월당일수와 주당일수를 이용해서 해당월의 week  count 를 구한다 - 2. 남은 요일 빈칸은 다음달로 넘긴다 - 3. 이전달의 남은 빈칸 + 월당일수 로 해당월의 week count 를 구한다 - 4. 2~3의 과정을 반복해서 week count를 모두 더한다
    - small set 에서는 동작이 되나, large set에서는 out of time 이다.

--- New Calendar/NewCalendar.go
+++ New Calendar/NewCalendar.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -16,14 +17,31 @@ func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 
 	scanner.Scan()
-	lineCount := scanner.Text()
-	fmt.Print("lineCount ", lineCount, "\n")
+	// lineCount := scanner.Text()
+	// fmt.Print("lineCount ", lineCount, "\n")
 
 	i := 1
 	for scanner.Scan() {
 		line := scanner.Text()
 		parts := strings.Split(line, " ")
-		fmt.Print("[", i, "] ", parts, "\n")
+
+		totalMonth, _ := strconv.ParseInt(parts[0], 10, 64)
+		daysOfMonth, _ := strconv.ParseInt(parts[1], 10, 64)
+		daysOfWeek, _ := strconv.ParseInt(parts[2], 10, 64)
+
+		fmt.Print("Case #", i, ": ", lineOfCalendar(totalMonth, daysOfMonth, daysOfWeek), "\n")
 		i++
 	}
+}
+
+func lineOfCalendar(totalMonth int64, daysOfMonth int64, daysOfWeek int64) int64 {
+	var lines int64 = 0
+	var prev int64 = 0
+	var i int64 = 0
+	for ; i < totalMonth; i++ {
+		lines += ((prev + daysOfMonth) + (daysOfWeek - 1)) / daysOfWeek
+		prev = (prev + daysOfMonth) % daysOfWeek
+	}
+
+	return lines
 }
