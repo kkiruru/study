0d4984275c3a69c544d9b8fa65132e024771483f
허현 <kkiruru@manty.co.kr>
2019-04-14 15:20:35 +0900

    연산자에 별로 func를 분리해서 처리

--- codejam/2012/restorebook/RestoreBook.go
+++ codejam/2012/restorebook/RestoreBook.go
@@ -17,12 +17,27 @@ func main() {
 	scanner.Scan()
 
 	i := 1
+	var formula string
+
 	for scanner.Scan() {
 		line := scanner.Text()
 		parts := strings.Split(line, " ")
+		if parts[1] == "+" {
+			formula = addition(parts[0], parts[2], parts[4])
+		} else {
+			formula = subtraction(parts[0], parts[2], parts[4])
+		}
 
-		fmt.Print("Case #", i, ": ", parts, "\n")
+		fmt.Print("Case #", i, ": ", formula, "\n")
 
 		i++
 	}
+}
+
+func addition(augend string, addend string, sum string) string {
+	return (augend + " + " + addend + " = " + sum)
+}
+
+func subtraction(minuend string, subtrahend string, difference string) string {
+	return (minuend + " - " + subtrahend + " = " + difference)
 }
