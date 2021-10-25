99ab3d07ed2f6e960c0591ca5592004770f5a0ed
허현 <kkiruru@gmail.com>
2021-10-25 15:50:33 +0900

    6. 열거형과 패턴 매칭

---
+++ rust/the book/6.enumerations/src/main.rs
@@ -0,0 +1,191 @@
+fn main() {
+    println!("Hello, world!");
+
+    defining(); //6.1 Defining an Enum
+
+    match_control(); //6.2 The match Control Flow Operator
+
+    if_control(); //6.3 Concise Control Flow with if let
+}
+
+fn defining() {
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+
+    route(IpAddrKind::V4);
+    route(IpAddrKind::V6);
+
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+
+    let home = IpAddr2::V4(String::from("127.0.0.1"));
+    let loopback = IpAddr2::V6(String::from("::1"));
+
+    let home = IpAddr3::V4(127, 0, 0, 1);
+    let loopback = IpAddr3::V6(String::from("::1"));
+
+    let m = Message::Write(String::from("hello"));
+    m.call();
+
+    let some_number = Some(5);
+    let some_string = Some("a string");
+
+    let absent_number: Option<i32> = None;
+
+
+    let x: i8 = 5;
+    let y: Option<i8> = Some(5);
+
+    // let sum = x + y;
+    let sum = x + y.unwrap();
+}
+
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
+fn route(ip_type: IpAddrKind) {}
+
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
+enum IpAddr2 {
+    V4(String),
+    V6(String),
+}
+
+enum IpAddr3 {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+// struct Ipv4Addr {
+//     //
+// }
+
+// struct Ipv6Addr {
+//     //
+// }
+
+// enum IpAddr {
+//     V4(Ipv4Addr),
+//     V6(Ipv6Addr),
+// }
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+struct QuitMessage;
+struct MoveMessage {
+    x: i32,
+    y: i32,
+}
+
+struct WriteMessage(String);
+struct ChangeColorMessage(i32, i32, i32);
+
+impl Message {
+    fn call(&self) {
+        //
+    }
+}
+
+fn match_control() {
+    value_in_cents(Coin::Quarter(UsState::Alaska));
+
+    let five = Some(5);
+    let six = plus_one(five);
+    println!("{}", six.unwrap());
+
+    let none = plus_one(None);
+    println!("{}", none.unwrap_or_default());
+
+    let some_u8_value = 0u8;
+    match some_u8_value {
+        1 => println!("one"),
+        3 => println!("three"),
+        5 => println!("five"),
+        7 => println!("seven"),
+        _ => (),
+    }
+}
+
+#[derive(Debug)]
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}", state);
+            25
+        }
+    }
+}
+
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
+fn if_control() {
+    let some_u8_value = Some(0u8);
+    match some_u8_value {
+        Some(3) => println!("three"),
+        _ => (),
+    }
+
+    if let Some(3) = some_u8_value {
+        println!("three");
+    }
+
+    let mut count = 0;
+    let coin = Coin::Dime;
+    match coin {
+        Coin::Quarter(state) => println!("State quarter from {:?}", state),
+        _ => count += 1,
+    };
+    println!("count {}", count);
+
+
+    let mut count = 0;
+    let coin = Coin::Dime;
+    if let Coin::Quarter(state) = coin {
+        println!("State quarter from {:?}!", state);
+    } else {
+        count += 1;
+    }
+    println!("count {}", count);
+}
