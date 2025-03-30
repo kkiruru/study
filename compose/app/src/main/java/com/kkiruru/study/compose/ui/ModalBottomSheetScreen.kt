a66087ded94faf7b98eac3e972a0c86d67845248
Hyun Heo <kkiruru@gmail.com>
2025-03-30 16:56:30 +0900

    sdk version-up

--- compose/app/src/main/java/com/kkiruru/study/compose/ui/ModalBottomSheetScreen.kt
+++ compose/app/src/main/java/com/kkiruru/study/compose/ui/ModalBottomSheetScreen.kt
@@ -1,27 +1,13 @@
 package com.kkiruru.study.compose.ui
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ExtendedFloatingActionButton
-import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.launch
 
 @Composable
 fun ModalBottomSheetScreenRoute() {
@@ -33,49 +19,10 @@ fun ModalBottomSheetScreenRoute() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun ModalBottomSheetScreen() {
-    val sheetState = androidx.compose.material3.rememberModalBottomSheetState(
+    val sheetState = rememberModalBottomSheetState(
         skipPartiallyExpanded = true
     )
     val scope = rememberCoroutineScope()
     var showBottomSheet by remember { mutableStateOf(false) }
 
-    Scaffold(
-        floatingActionButton = {
-            ExtendedFloatingActionButton(
-                text = { Text("Show bottom sheet") },
-                icon = { Icon(Icons.Filled.Add, contentDescription = "") },
-                onClick = {
-                    showBottomSheet = true
-                }
-            )
-        }
-    ) { contentPadding ->
-        // Screen content
-
-        if (showBottomSheet) {
-            ModalBottomSheet(
-                onDismissRequest = {
-                    showBottomSheet = false
-                },
-                sheetState = sheetState
-            ) {
-
-                Box(
-                    modifier = Modifier.fillMaxSize()
-                ) {
-                    // Sheet content
-                    Button(onClick = {
-                        scope.launch { sheetState.hide() }.invokeOnCompletion {
-                            if (!sheetState.isVisible) {
-                                showBottomSheet = false
-                            }
-                        }
-                    }) {
-                        Text("Hide bottom sheet")
-                        Spacer(modifier = Modifier.height(100.dp))
-                    }
-                }
-            }
-        }
-    }
 }
